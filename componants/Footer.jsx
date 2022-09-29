d0151701cb471592d5e19fd210acfc1fa123dde1
Javed <mujawar.javadu@gmail.com>
2022-09-29 12:15:42 +0530

    New template added.

--- componants/Footer.jsx
+++
@@ -1,7 +0,0 @@
-import { Layout } from 'antd';
-const {Footer} = Layout;
-
-export default function FooterPage (){
-
-    return <Footer>Footer</Footer>
-}
--- componants/Header.jsx
+++
@@ -1,7 +0,0 @@
-import { Layout } from 'antd';
-const { Header, Footer, Sider, Content } = Layout;
-
-export default function HeaderPage(){
-
-    return <Header>Header</Header>
-}
